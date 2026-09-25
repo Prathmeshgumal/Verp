6eb18e688322d05df88267dea6970262278212a5
Prathmesh Gumal <prathmeshgumal@gmail.com>
2026-09-25 22:13:18 +0530

    mark forgotten checkouts as missed automatically

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,17 @@
 import { buildApp } from './app';
 import { loadConfig } from './config';
 import { createDb } from './db/client';
+import { startMissedCheckoutJob } from './jobs/close-missed-checkouts';
+import { systemClock } from './lib/clock';
 
 const config = loadConfig();
 const { db, pool } = createDb(config.DATABASE_URL);
 const app = await buildApp({ config, db });
+const stopJobs = startMissedCheckoutJob({ db, clock: systemClock, log: app.log });
 
 const shutdown = async (signal: string) => {
   app.log.info({ signal }, 'shutting down');
+  stopJobs();
   await app.close();
   await pool.end();
   process.exit(0);
