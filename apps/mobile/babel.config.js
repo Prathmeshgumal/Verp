a2cb29f05fb2cd7783ebd40bf0b4756e773bc39d
Prathmesh Gumal <prathmeshgumal@gmail.com>
2026-09-26 12:46:17 +0530

    fix app bundle failing on shared validation library

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  // zod v4 (via @ve/shared) ships `export * as ns from`, which the RN preset does not transform.
+  plugins: ['@babel/plugin-transform-export-namespace-from'],
 };
